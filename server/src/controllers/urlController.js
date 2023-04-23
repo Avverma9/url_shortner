32dac85872398a80a4fcec687eb5b92bbc076cbb
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-04-23 22:26:28 +0530

    redis client is closed

--- server/src/controllers/urlController.js
+++ server/src/controllers/urlController.js
@@ -4,7 +4,7 @@ const axios = require ('axios')
 const shortid = require ('shortid')
 const {promisify} = require("util")
 const urlModel = require("../models/urlModel")
-
+const { createClient } = require('redis') ;
 // code for validation
 
 const validation = function(value){
@@ -16,86 +16,80 @@ const validation = function(value){
 }
 
 //================redis config=======================//
-const redisConfig= redis.createClient(
-    17763,
-    "redis-17763.c305.ap-south-1-1.ec2.cloud.redislabs.com",
-    {no_ready_check: true}
+const client = createClient({
+  password: 'BzpeyuADtkzR34QaCQl6hfICsmQMra0B',
+  socket: {
+      host: 'redis-11459.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+      port: 11459
+  }
+});
 
-)
-redisConfig.auth("aCzvJ5E21jJs1JinkECCude94VnCLCQp", function (err) {
-    if (err) throw err;
-  });
-  
-  redisConfig.on("connect", async function () {
-    console.log("Redis is Connected...");
-  });
-  
-  const GET_ASYNC = promisify(redisConfig.GET).bind(redisConfig);
-  const SET_ASYNC = promisify(redisConfig.SETEX).bind(redisConfig);
+client.on("connect", async function () {
+console.log("Redis is Connected...");
+});
+
+const GET_ASYNC = promisify(client.get).bind(client);
+const SET_ASYNC = promisify(client.setEx).bind(client);
+
+
 //=================creating short url========================//
-const urlShort = async (req, res) => {
-    try {
-      const { longUrl } = req.body;
-  
-      if (!longUrl) {
-        return res.status(400).send({
-          status: false,
-          message: "It seems you forgot to enter the long URL",
-        });
-      }
-  
-      if (!validation(longUrl)) {
-        return res.status(400).send({
-          status: false,
-          message: "Sorry, it wouldn't work! Provide a valid URL",
-        });
-      }
-  
-      const cachedData = await GET_ASYNC(longUrl);
-  
-      if (cachedData) {
-        const result = JSON.parse(cachedData);
-  
-        return res.status(201).send({ status: true, data: result });
-      }
-  
-      const [verifyURL, findData] = await Promise.all([
-        axios
-          .get(longUrl)
-          .then(() => [])
-          .catch(() => null),
-        urlmodel.findOne({ longUrl }).select({ _id: 0, __v: 0 }),
-      ]);
-  
-      if (!verifyURL) {
-        return res
-          .status(400)
-          .send({ status: false, message: `The link you provided ${longUrl} seems not valid` });
-      }
-  
-      const shortenURL = shortid.generate().toLowerCase();
-      const baseURL = "http://localhost:3000/";
-      const obj = {
-        longUrl: longUrl.trim(),
-        shortUrl: baseURL + shortenURL,
-        urlCode: shortenURL,
-      };
-  
-      if (findData) {
-        await SET_ASYNC(longUrl, 60 * 5, JSON.stringify(findData));
-  
-        return res.status(201).send({ status: true, message: findData });
-      }
-  
-      const final = await urlmodel.create(obj);
-      await SET_ASYNC(longUrl, 60 * 5, JSON.stringify(obj));
-  
-      return res.status(201).send({ status: true, data: obj });
-    } catch (err) {
-      return res.status(500).send({ status: false, message: err.message });
-    }
-  };
-  
+const urlShort = async(req,res) => {
+  try{
+      let data = req.body
+      let{longUrl}=data
+
+      if(Object.keys(data).length == 0) return res.status(400).send({status : false, message : "Oops You didn't entered anything"})
+
+      if(!longUrl) return res.status(400).send({status : false, message : "It seem's you forgot to enter long url"})
+
+      if(!validation(longUrl)) return res.status(400).send({status : false, message : "Sorry, It wouldn't work ! Provide Valid URL"})
+
+     let cachedData = await GET_ASYNC(`${longUrl}`)
+
+     if(cachedData){
+
+      let result = JSON.parse(cachedData)
+
+      return res.status(201).send({status: true, data: result})
+      
+     }
+
+     let fetchType ={
+      method: "get",
+      url: longUrl,
+     }
+
+     let verifyURL = await axios(fetchType)
+     .then(() => [])
+     .catch(() => null)
+
+     if(!verifyURL){
+      return res.status(400).send({status: false, message : `The Link You Provide ${longUrl} seem's not valid`})
+     } 
+
+     let shortenURL =shortid.generate().toLowerCase()
+     let baseURL ="http://localhost:3000/"
+     let obj ={
+      longUrl: longUrl.trim(),
+      shortUrl: baseURL + shortenURL,
+      urlCode : shortenURL
+     }
+
+     let findData= await urlmodel.findOne({ longUrl : longUrl}).select({_id:0, __v:0});
+     if(findData){
+     await SET_ASYNC(`${longUrl}`, 60 * 5, JSON.stringify(findData))
+     return res.status(201).send({status : true, message: findData})}
+
+     let final= await urlmodel.create(obj)
+     await SET_ASYNC(`${longUrl}`, 60 * 5, JSON.stringify(obj) )
+    return res.status(201).send({status: true, data: obj})
+
+  } catch(err){
+      return res.status(500).send({status : false, message: err.message})
+  }
+ 
+
+}
 //------------------------------ Getting Data ----------------------------------------------//
 
 let fetchData = async(req,res) => {
